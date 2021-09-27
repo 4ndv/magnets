3a15e0369ad1f50c2428904676b58dd1d7837584
Andrey Viktorov <andreyviktorov@outlook.com>
2021-09-27 23:41:34 +0700

    fix: semicolons, title and description

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from 'next/head'
+import Head from "next/head"
 import { Container } from "@chakra-ui/react"
 
 export default function Home() {
   return (
     <Container>
       <Head>
-        <title>Magnets - online magnet links editor</title>
-        <meta name="description" content="Magnets is a visual magnet link generator, editor and parser" />
+        <title>Magnets - Online Magnet Links Editor</title>
+        <meta name="description" content="Magnets is a visual magnet link generator, editor and parser. Convert torrent files to magnets and vice versa, edit and import trackers, and much more" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>Hello!</div>

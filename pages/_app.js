edad7c00763de7350cfaf3d1ada430663c9626f2
Andrey Viktorov <andreyviktorov@outlook.com>
2021-09-29 05:42:45 +0700

    fix: pass theme to provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@/lib/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );

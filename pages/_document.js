a7c821da31419f054ae30d1dc0f53f608cf31e66
Andrey Viktorov <loonks@disroot.org>
2025-07-03 22:41:51 +0700

    chore: replace ackee with umami

--- pages/_document.js
+++ pages/_document.js
@@ -15,11 +15,9 @@ export default class Document extends NextDocument {
           <NextScript />
 
           <script
-            async
-            src="https://a.shirou.lynx.pink/tracker.js"
-            data-ackee-server="https://a.shirou.lynx.pink"
-            data-ackee-domain-id="93e65cd3-a55b-41e7-957e-d6f51b25b43b"
-            data-ackee-opts='{ "detailed": true }'
+            defer
+            src="https://umami.lynx.st/script.js"
+            data-website-id="48405134-ce77-4387-a6a3-81e4e5367bee"
           />
         </body>
       </Html>
